aa3c559ea84f64cbb48ec95aaee9e29868fbc883
David Cotelessa <David@HIX00134.local>
2018-01-29 11:12:35 -0800

    master: handles onChange onBlur better

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 export default class EditInline extends Component {
   static propTypes = {
     blurOnFinish: PropTypes.bool,
-    children: PropTypes.node,
+    children: PropTypes.object,
     component: PropTypes.any.isRequired,
     componentProps: PropTypes.object,
     disabled: PropTypes.bool,
     extraParams: PropTypes.object,
     name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
+    onChange: PropTypes.func,
     onFormat: PropTypes.func,
     style: PropTypes.object,
     validate: PropTypes.func,
@@ -22,7 +22,8 @@ export default class EditInline extends Component {
     children: undefined,
     componentProps: {},
     disabled: false,
-    extraParams: null,
+    extraParams: {},
+    onChange: null,
     onFormat: null,
     style: {},
     validate: null,
@@ -42,9 +43,16 @@ export default class EditInline extends Component {
   onFieldBlur = (e, extraParams) => {
     const { onChange, validate, name } = this.props
     if (onChange) {
-      if (!validate || validate(e.target.value)) {
+      if (typeof validate === 'function') {
+        if (validate(e.target.value)) {
+          onChange(e.target.value, name, extraParams)
+        }
+      } else {
         onChange(e.target.value, name, extraParams)
       }
+    }
+    if (extraParams.onBlur) {
+      extraParams.onBlur(e)
     }
     this.onFinishEditing()
   }
@@ -54,12 +62,13 @@ export default class EditInline extends Component {
    * @param {Object} e change event of input
    * @param {boolean} blurOnFinish if the input finished after updating its value
    */
-  onFieldChange = (e, blurOnFinish) => {
+  onFieldChange = (e, blurOnFinish, extraParams) => {
     this.setState({ value: e.target.value })
     if (blurOnFinish) {
-      this.props.onChange()
+      this.onFieldBlur(e, extraParams)
       this.onFinishEditing()
     }
+    if (extraParams.onChange) extraParams.onChange(e)
   }
 
   // enables the component to be editable using the editable state
@@ -87,7 +96,6 @@ export default class EditInline extends Component {
       extraParams,
       onFormat,
       style,
-      value,
     } = this.props
     const InlineComponent = component
 
@@ -96,8 +104,12 @@ export default class EditInline extends Component {
       disabled,
       onBlur: e => (blurOnFinish ? null : this.onFieldBlur(e, extraParams)),
       autoFocus: true,
-      value: this.state.value || value,
+      value: this.state.value,
       ...componentProps,
+    }
+
+    if (blurOnFinish) {
+      fieldProps.finishEdit = this.onFinishEditing
     }
 
     if (!this.state.editable) {
@@ -106,7 +118,7 @@ export default class EditInline extends Component {
           onClick={disabled ? null : this.onEditEnable}
           style={style}
         >
-          {onFormat ? onFormat(fieldProps, children) : children}
+          {onFormat ? onFormat(fieldProps) : children}
         </span>
       )
     }
